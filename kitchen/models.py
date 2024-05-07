dd65d657bf5f9e3498bf36d23c47b4c63eac66b9
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-05-07 12:37:51 +0300

    Added DishType model

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name", ]
+
+    def __str__(self):
+        return self.name

d674d40fc63a5c7fe067530f0e1c006c5291cc82
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-05-07 12:42:04 +0300

    Added Cook model

--- kitchen/models.py
+++ kitchen/models.py
@@ -11,3 +11,15 @@ class DishType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(max_length=255)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+

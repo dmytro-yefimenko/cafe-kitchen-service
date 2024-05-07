c154127f4eccd8a7bc428cf0c8b9c5331d73ac12
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-05-07 13:03:27 +0300

    Added Dish model

--- kitchen/models.py
+++ kitchen/models.py
@@ -23,3 +23,13 @@ class Cook(AbstractUser):
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
 
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True, default="No description yet.")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    def __str__(self):
+        return self.name

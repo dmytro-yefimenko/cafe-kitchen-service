d38aad4f184306145c49f224c291f399467a4967
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-05-08 08:18:58 +0300

    Added index view for index.html in views.py

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from models import DishType, Cook, Dish
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+        "num_visits": num_visits + 1
+    }
+
+    return render(request, "kitchen/index.html", context=context)

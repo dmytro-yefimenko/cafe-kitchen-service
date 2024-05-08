0ec80c0b53aa657d52c607337308ebff15e4f4e5
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-05-08 08:50:17 +0300

    Added urlpattern for index and corrected a typo in views.py

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,2 +1,10 @@
 from django.urls import path
 
+from .views import (
+    index,
+)
+
+
+urlpatterns = [
+    path("", index, name="index"),
+]
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from models import DishType, Cook, Dish
+from .models import DishType, Cook, Dish
 
 
 @login_required

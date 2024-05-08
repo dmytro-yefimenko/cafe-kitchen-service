9c9130d1677445a846f0de8e0950f47a8f4d1ae2
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-05-08 17:36:30 +0300

    Added Create/Update/Delete for Dish Type in urls.py and views.py

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -2,11 +2,19 @@ from django.urls import path
 
 from .views import (
     index,
+    DishTypeListView,
+    DishTypeCreate,
+    DishTypeUpdate,
+    DishTypeDelete,
 )
 
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dish-types/create", DishTypeCreate.as_view(), name="dish-type-create"),
+    path("dish-types/<int:pk>/update", DishTypeUpdate.as_view(), name="dish-type-update"),
+    path("dish-types/<int:pk>/delete", DishTypeDelete.as_view(), name="dish-type-delete"),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from .models import DishType, Cook, Dish
+from .forms import DishTypeSearchForm
 
 
 @login_required
@@ -32,3 +34,36 @@ class DishTypeListView(LoginRequiredMixin, generic.ListView):
     context_object_name = "dish_type_list"
     template_name = "kitchen/dish_type_list.html"
     paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = DishTypeSearchForm(
+            initial={"name": name}
+        )
+
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        form = DishTypeSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
+
+
+class DishTypeCreate(LoginRequiredMixin, generic.CreateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish-type-list")
+
+
+class DishTypeUpdate(LoginRequiredMixin, generic.UpdateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish-type-list")
+
+
+class DishTypeDelete(LoginRequiredMixin, generic.DeleteView):
+    model = DishType
+    success_url = reverse_lazy("kitchen:dish-type-list")

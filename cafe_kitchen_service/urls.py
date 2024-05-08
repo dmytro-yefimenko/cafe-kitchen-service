9dded9022105748693ef86245a8d9674a6c31b0b
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-05-08 08:37:40 +0300

    Fixed typos in index.html and app urls.py

--- cafe_kitchen_service/urls.py
+++ cafe_kitchen_service/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("kitchen.urls", namespace="taxi")),
+    path("", include("kitchen.urls", namespace="kitchen")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
